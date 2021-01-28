7baf1cdcadc0206247496a82bd04de7eb155f6de
Flávio Motta <flaviovr@gmail.com>
2021-01-27 23:32:16 -0300

    layout

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 export const QuizContainer = styled.div`
   width: 100%;
-  max-width: 400px;
+  max-width: 500px;
   padding-top: 45px;
   margin: auto 10%;
   @media screen and (max-width: 880px) {
     margin: 0 auto;
     padding: 15px;
     width: 100%;
-    max-width: 100%;
+    max-width: 350px;
   }
 `;
 
